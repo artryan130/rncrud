c44c3d972ca3e8fd61ad298932cef69ae6089b65
artryan130 <80287857+artryan130@users.noreply.github.com>
2022-04-18 17:53:15 -0300

    Lista de usuários

---
+++ src/data/users.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        name: 'Joao da Silva',
+        email: 'joao@ufmg.br',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png'
+    },
+    {
+        id: 2,
+        name: 'Maria das Dores',
+        email: 'maria@ufmg.br',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_960_720.png'
+    },
+    {
+        id: 3,
+        name: 'Pedro Alvares',
+        email: 'pedro@ufmg.br',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_960_720.png'
+    },
+    {
+        id: 4,
+        name: 'Lucas Lira',
+        email: 'lucas@ufmg.br',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/08/18/11/00/man-1602633_960_720.png'
+    },
+]
